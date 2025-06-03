bf42f42360d9827bf0103d72c6085f73d320f273
Tomás Ariel Conti <taconti02@gmail.com>
2025-06-03 11:40:16 -0300

    Cambios 3/6/2025 11:40 hs
    Agregue en registrar el estilo mejorado

--- app/Views/contenido/registrarse.php
+++ app/Views/contenido/registrarse.php
@@ -14,34 +14,34 @@
             </div>
         <?php endif; ?>
 
-    <form action="<?php echo base_url('registro_cliente') ?>" method="post" autocomplete="off">
-
-        <div class="col-md-5 mt-3">
+    <form action="<?php echo base_url('registro_cliente') ?>" method="post" autocomplete="off" class="card text-center formulario-login container-fluid mb-5" style="width: 45rem; margin-top: 5%">
+    <div class="d-flex flex-column align-items-center">
+        <div class="col-md-5 mt-3 w-100">
             <label for="nombre" class="form-label">Nombre</label>
             <?php echo form_input(['name' => 'nombre', 'id' => 'nombre1', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese nombre', 'value' => set_value('nombre')]); ?>
         </div>
 
-        <div class="col-md-5 mt-3">
+        <div class="col-md-5 mt-3 w-100">
             <label for="apelido" class="form-label">Apellido</label>
             <?php echo form_input(['name' => 'apellido', 'id' => 'apellido1', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese apellido', 'value' => set_value('apellido')]); ?>
         </div>
         
-        <div class="col-md-6 mt-3">
+        <div class="col-md-5 mt-3 w-100">
             <label for="email" class="form-label">Email</label>
             <?php echo form_input(['name' => 'correo', 'id' => 'correo1', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese correo electronico', 'value' => set_value('correo')]); ?>
         </div>
         
-        <div class="col-md-5 mt-3">
+        <div class="col-md-5 mt-3 w-100">
             <label for="pais" class="form-label">País</label>
             <?php echo form_input(['name' => 'pais', 'id' => 'pais1', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese pais', 'value' => set_value('pais')]); ?>
         </div>
         
-        <div class="col-md-5 mt-3"> 
+        <div class="col-md-5 mt-3 w-100"> 
             <label for="contraseña" class="form-label">Contraseña</label>
             <?php echo form_input(['name' => 'contraseña', 'id' => 'contraseña1', 'type' => 'password', 'class' => 'form-control', 'placeholder' => 'Ingrese contraseña', 'value' => set_value('contraseña')]); ?>
         </div>
         
-        <div class="col-md-5 mt-3">
+        <div class="col-md-5 mt-3 w-100">
             <label for="reContraseña" class="form-label">Repetir contraseña</label>
             <?php echo form_input(['name' => 'reContraseña', 'id' => 'reContraseña1', 'type' => 'password', 'class' => 'form-control', 'placeholder' => 'Repetir contraseña', 'value' => set_value('reContraseña')]); ?>
         </div>
@@ -49,10 +49,10 @@
         <div class="col-2 mt-4">
             <?php echo form_submit('registrarse', 'Finalizar', "class='btn btn-primary' type='submit'"); ?>
         </div>
-        
+      </div>  
     </form>
     <br>
-    <div style="margin-bottom: 10px; margin-top: 10px; margin-left: 45%">
+    <div class="card text-center formulario-login container-fluid mb-5" style="width: 45rem; margin-top: 5%">
         <a class="btn btn-primary" href="<?php echo base_url('/'); ?>" role="button">Volver al inicio</a>
     </div>
     
