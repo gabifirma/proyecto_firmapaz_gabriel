64d1239d2c8a07a255f140cc95856dc430937f6c
Tomás Ariel Conti <taconti02@gmail.com>
2025-06-12 09:15:33 -0300

    "acepto cambios 2.0"

--- app/Views/Backend/editar_juego.php
+++ app/Views/Backend/editar_juego.php
@@ -1,74 +1,74 @@
-<h1 class="text-center mt-5">Editar Juegos</h1>
-    <div class="container-fluid">
-        <div class="w-50 mx-auto" id="contacto">
-            
-            <?php if (!empty($validation)): ?>
-                <div class="alert alert-danger">
-                    <ul>
-                        <?php foreach ($validation as $error): ?>
-                            <li><?= esc($error) ?></li>
-                        <?php endforeach?>
-                    </ul>
-                </div>
-            <?php endif; ?>
-            
-            <?php if(session('mensaje')): ?>
-                <div class="alert alert-success">
-                    <?= session('mensaje'); ?>
-                </div>
-            <?php endif; ?>
 
-            <?php echo form_open_multipart('actualizar_videojuego/'.$videojuego['id_videojuego'])?>
 
-                <div class="form-group mt-3">
-                    <label for="titulo">Título del juego</label>
-                    <?php echo form_input(['name' => 'titulo', 'id' => 'titulo', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese título del juego', 'value' => $videojuego["titulo_videojuego"] ]);?>
-                </div>
-
-                <div class="form-group mt-3">
-                    <label for="descripcion">Descripción del juego</label>
-                    <?php echo form_input(['name' => 'descripcion', 'id' => 'descripcion', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese la descripción del juego', 'value' => $videojuego["descripcion_videojuego"] ]); ?>
-                </div>
-
-                <div class="form-group mt-3">
-                    <label for="desarrollador">Desarrollador del juego</label>
-                    <?php echo form_input(['name' => 'desarrollador', 'id' => 'desarrollador', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el desarrollador del juego', 'value' => $videojuego["desarrollador_videojuego"] ]); ?>
-                </div>
-
-                <div class="form-group mt-3">
-                    <label for="distribuidor">Distribuidor del juego</label>
-                    <?php echo form_input(['name' => 'distribuidor', 'id' => 'distribuidor', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el distribuidor del juego', 'value' => $videojuego["distribuidor_videojuego"] ]); ?>
-                </div>
-
-                <div class="form-group mt-3">
-                    <label for="precio">Precio del juego ($)</label>
-                    <?php echo form_input(['name' => 'precio', 'id' => 'precio', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el precio del juego', 'value' => $videojuego["precio_videojuego"] ]); ?>
-                </div>
-
-                <div class="form-group mt-3">
-                    <label for="categoria">Categoria del juego</label>
-                    <?php 
-                        $lista['0'] = 'Seleccione la categoria';
-                        foreach($categoria as $row){
-                            $categoria_id = $row['id_categoria'];
-                            $categoria_desc = $row['categoria_descripcion'];
-                            $lista[$categoria_id] = $categoria_desc;
-                        }
-                        echo form_dropdown('categoria', $lista, '0', 'class="form-control"');
-                    ?>
-                </div>
+    <h1 class="text-center mt-5">Editar Juegos</h1>
+        <div class="container-fluid">
+            <div class="w-50 mx-auto" id="contacto">
                 
-                <div class="form-group mt-3">
-                    <label for="imagen">Imagen del juego</label>
-                    <?php echo form_upload(['name' => 'imagen', 'id' => 'imagen', 'class' => 'form-control']); ?>
-                </div>
+                <?php if (!empty($validation)): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php foreach ($validation as $error): ?>
+                                <li><?= esc($error) ?></li>
+                            <?php endforeach?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
                 
-                <div class="form-group mt-3">
-                    <?php echo form_submit('Actualizar', 'Actualizar', 'class="btn btn-success"'); ?>
-                </div>
+                <?php if(session('mensaje')): ?>
+                    <div class="alert alert-success">
+                        <?= session('mensaje'); ?>
+                    </div>
+                <?php endif; ?>
 
-            <?php echo form_close();?>
+                <?php echo form_open_multipart('actualizar_videojuego/'.$videojuego['id_videojuego'])?>
+
+                    <div class="form-group mt-3">
+                        <label for="titulo">Título del juego</label>
+                        <?php echo form_input(['name' => 'titulo', 'id' => 'titulo', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese título del juego', 'value' => $videojuego["titulo_videojuego"] ]);?>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <label for="descripcion">Descripción del juego</label>
+                        <?php echo form_input(['name' => 'descripcion', 'id' => 'descripcion', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese la descripción del juego', 'value' => $videojuego["descripcion_videojuego"] ]); ?>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <label for="desarrollador">Desarrollador del juego</label>
+                        <?php echo form_input(['name' => 'desarrollador', 'id' => 'desarrollador', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el desarrollador del juego', 'value' => $videojuego["desarrollador_videojuego"] ]); ?>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <label for="distribuidor">Distribuidor del juego</label>
+                        <?php echo form_input(['name' => 'distribuidor', 'id' => 'distribuidor', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el distribuidor del juego', 'value' => $videojuego["distribuidor_videojuego"] ]); ?>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <label for="precio">Precio del juego ($)</label>
+                        <?php echo form_input(['name' => 'precio', 'id' => 'precio', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el precio del juego', 'value' => $videojuego["precio_videojuego"] ]); ?>
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <label for="categoria">Categoria del juego</label>
+                        <?php 
+                            $lista['0'] = 'Seleccione la categoria';
+                            foreach($categoria as $row){
+                                $categoria_id = $row['id_categoria'];
+                                $categoria_desc = $row['categoria_descripcion'];
+                                $lista[$categoria_id] = $categoria_desc;
+                            }
+                            echo form_dropdown('categoria', $lista, '0', 'class="form-control"');
+                        ?>
+                    </div>
+                    
+                    <div class="form-group mt-3">
+                        <label for="imagen">Imagen del juego</label>
+                        <?php echo form_upload(['name' => 'imagen', 'id' => 'imagen', 'class' => 'form-control']); ?>
+                    </div>
+                    
+                    <div class="form-group mt-3">
+                        <?php echo form_submit('Actualizar', 'Actualizar', 'class="btn btn-success"'); ?>
+                    </div>
+
+                <?php echo form_close();?>
+            </div>
         </div>
-    </div>
-
-   
