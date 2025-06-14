f3d42c7b87583981f8d0a980bbc733ec4cbfdbaf
gabifirma <gabfirmapaz@gmail.com>
2025-06-14 17:39:41 -0300

    arreglos 14/6/25

--- app/Controllers/Carrito_controller.php
+++ app/Controllers/Carrito_controller.php
@@ -58,7 +58,7 @@ class Carrito_controller extends BaseController{
         }
 
         $data = array(
-            'persona_id' => session('id'),
+            'id_persona' => session('id'),
             'fecha_venta' => date('Y-m-d'),
             'total_venta' => session('total'),
         );
