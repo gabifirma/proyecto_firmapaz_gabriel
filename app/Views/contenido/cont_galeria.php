970941baebc8bf22310a97b90fd746bfe4c89c23
gabifirma <gabfirmapaz@gmail.com>
2025-06-17 11:57:42 -0300

    arreglos 3 16/6/25

--- app/Views/contenido/cont_galeria.php
+++ app/Views/contenido/cont_galeria.php
@@ -7,20 +7,10 @@
                 <img src="<?= base_url('assets/img/' . $juego['imagen_videojuego']) ?>" class="img-fluid card-img-top" alt="">
                 <div class="capa">
                     <h3><strong><?= esc($juego['titulo_videojuego']) ?></strong></h3><br>
-                    <p><strong>Desarrollador:</strong> <?= esc($juego['desarrollador_videojuego']) ?></p>
-                    <p><strong>Publicado por:</strong> <?= esc($juego['distribuidor_videojuego']) ?></p>
-                </div>
-                <a href="#" class="stretched-link"></a>
+                </div>             
               </div>
+              <a href="<?php echo base_url('ver_juego/'. $juego['id_videojuego']) ?>" class="btn btn-success mt-4" role="button">Ver detalles</a> 
               <br>
-              <?php if(session('login')){
-                  echo form_open('add_cart');
-                    echo form_hidden('id', $juego['id_videojuego']);
-                    echo form_hidden('titulo', $juego['titulo_videojuego']);
-                    echo form_hidden('precio', $juego['precio_videojuego']);
-                    echo form_submit('comprar', 'Agregar al carrito', "class='btn btn-success'; style='text-center'");
-                    echo form_close();
-                } ?>
           </div>
         <?php endforeach; ?>
     </div>
---
+++ app/Views/contenido/ver_juego.php
@@ -0,0 +1,29 @@
+<div class="juego-detalle-flex">
+    <aside>
+        <div>
+            <img src="<?= base_url('assets/img/' . $juego['imagen_videojuego']) ?>" class="img-fluid card-img-top" alt="">
+        </div> 
+    </aside>
+    <section>
+        <div">
+            <h3><strong>Título:</strong> <?= esc($juego['titulo_videojuego']) ?></h3> 
+            <h3><strong>Desarrollador:</strong> <?= esc($juego['desarrollador_videojuego']) ?></h3>
+            <h3><strong>Distribuidor:</strong> <?= esc($juego['distribuidor_videojuego']) ?></h3>
+            <h3><strong>Título:</strong> <?= esc($juego['titulo_videojuego']) ?></h3>
+            <h3><strong>Precio:</strong> $<?= esc($juego['precio_videojuego']) ?></h3>
+            <h3><strong>Categoria:</strong> <?= esc($juego['titulo_videojuego']) ?></h3>
+            <h3><strong>Stock:</strong> <?= esc($juego['videojuego_stock']) ?> Unidades</h3>
+            <h4><strong>Descripción:</strong> <?= esc($juego['descripcion_videojuego']) ?></h4>
+        </div>
+        <div style="text-align: center; margin-top: 20px;">
+            <?php if(session('login')){
+                echo form_open('add_cart');
+                echo form_hidden('id', $juego['id_videojuego']);
+                echo form_hidden('titulo', $juego['titulo_videojuego']);
+                echo form_hidden('precio', $juego['precio_videojuego']);
+                echo form_submit('comprar', 'Agregar al carrito', "class='btn btn-success'; style='text-center'");
+                echo form_close();
+            } ?>
+        </div> 
+    </section>
+</div>
