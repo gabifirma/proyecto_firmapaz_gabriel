ed81d38e0bdf63efbad10cceab5c73ab054e78a2
gabifirma <gabfirmapaz@gmail.com>
2025-06-19 18:39:27 -0300

    arreglo final 19/6/25

--- app/Views/contenido/ver_categoria.php
+++ app/Views/contenido/ver_categoria.php
@@ -1,9 +1,28 @@
 <section class="mt-4 container">
+
+    <div class="row row-cols-1 row-cols-md-5 g-1 mt-2" id="botones">
+      <div class="col">
+        <a href="<?php echo base_url('ver_categoria/4'); ?>"><button type="button" class="btn btn-outline-success btn-lg"><span class="fa-solid--chess"></span><br>Estrategia</button></a>
+      </div>
+      <div class="col">
+        <a href="<?php echo base_url('ver_categoria/1'); ?>"><button type="button" class="btn btn-outline-success btn-lg"><span class="lucide--swords"></span><br>Acción</button></a>
+      </div>
+      <div class="col">
+        <a href="<?php echo base_url('ver_categoria/3'); ?>"><button type="button" class="btn btn-outline-success btn-lg"><span class="mdi--compass"></span><br>Aventura</button></a>
+      </div>
+      <div class="col">
+        <a href="<?php echo base_url('ver_categoria/5'); ?>"><button type="button" class="btn btn-outline-success btn-lg"><span class="iconoir--boxing-glove"></span><br>Pelea</button></a>
+      </div>
+      <div class="col">
+        <a href="<?php echo base_url('ver_categoria/2'); ?>"><button type="button" class="btn btn-outline-success btn-lg"><span class="iconoir--soccer-ball"></span><br>Deportes</button></a>
+      </div>
+    </div>
+
     <div class="mt-3 mb-3 ms-1">
         <?php if (session('login')): ?>
-            <a class="btn btn-outline-success" href="<?php echo base_url('catalogo_cliente'); ?>" role="button">Volver atrás</a>
+            <a class="btn btn-outline-success" href="<?php echo base_url('catalogo_cliente'); ?>" role="button">Volver a catálogo</a>
         <?php else: ?>
-            <a class="btn btn-outline-success" href="<?php echo base_url('galeria'); ?>" role="button">Volver atrás</a>
+            <a class="btn btn-outline-success" href="<?php echo base_url('galeria'); ?>" role="button">Volver a galería</a>
         <?php endif; ?>
     </div>
     <div class="row row-cols-1 row-cols-md-3 g-4">
